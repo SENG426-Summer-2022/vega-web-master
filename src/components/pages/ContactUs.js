1209f8d36c209074a1292c954f6a5438256ba334
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-27 12:06:47 -0700

    prettier, resolve code smells

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,29 +1,29 @@
-import FormPageLayout from '../templates/FormPageLayout.js';
-import ContactUsForm from '../UI/organisms/ContactUsForm.js';
+import FormPageLayout from "../templates/FormPageLayout.js";
+import ContactUsForm from "../UI/organisms/ContactUsForm.js";
 
 const onSubmit = async (name, email, message) => {
-	// post to server at localhost:8000/api/contact
-	// send name, email, and message
-	const response = await fetch('http://localhost:8000/api/contact', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			name: name,
-			email: email,
-			message: message
-		})
-	});
-	const result = await response.json();
-	return result;
-  };
+  // post to server at localhost:8000/api/contact
+  // send name, email, and message
+  const response = await fetch("http://localhost:8000/api/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      message: message,
+    }),
+  });
+  const result = await response.json();
+  return result;
+};
 
-const ContactUs = (props) => {
-	return (
-		<FormPageLayout>
-			<ContactUsForm onSubmit={onSubmit} />
-		</FormPageLayout>
-		);
-}
+const ContactUs = () => {
+  return (
+    <FormPageLayout>
+      <ContactUsForm onSubmit={onSubmit} />
+    </FormPageLayout>
+  );
+};
 export default ContactUs;
