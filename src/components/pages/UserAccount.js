490581e39ad888eb2ee2daf908f02c8d312cf4a1
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-27 13:37:58 -0700

    vault layout

--- src/components/pages/UserAccount.js
+++ src/components/pages/UserAccount.js
@@ -1,30 +1,41 @@
-import {useState, useContext} from 'react';
-import {useHistory} from "react-router-dom";
-import {Button, Row, Col} from 'react-bootstrap';
-import SimplePageLayout from '../templates/SimplePageLayout.js';
-import ListRoundedImages from '../UI/molecules/ListRoundImages.js';
-import {UserContext} from '../../auth/UserProvider.js';
-
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { Button, Row, Col } from "react-bootstrap";
+import SimplePageLayout from "../templates/SimplePageLayout.js";
+import { UserContext } from "../../auth/UserProvider.js";
 
 const UserAccount = (props) => {
-	const {user, setUserInfo,logout} = useContext(UserContext);
-	const history = useHistory();
-	
-	const logoutAndRouteChange = () => {
-		logout();
-		history.push("/");
-	}	
+  const { user, setUserInfo, logout } = useContext(UserContext);
+  const history = useHistory();
 
-	return (
-		<SimplePageLayout>
-			<Row>
-				<Col sm={6}>
-					<p>Hello,</p>
-					<p>{user.username}</p>
-					<Button onClick={logoutAndRouteChange} size="sm">signout</Button>
-				</Col>
-			</Row>
-		</SimplePageLayout>
-		);
-}
+  const logoutAndRouteChange = () => {
+    logout();
+    history.push("/");
+  };
+
+  const goToVault = () => {
+    history.push("/vault");
+  };
+
+  return (
+    <SimplePageLayout>
+      <Row>
+        <Col sm={6}>
+          <p>Hello,</p>
+          <p>{user.username}</p>
+          <Button
+            onClick={() => goToVault}
+            size="sm"
+            style={{ margin: "1rem" }}
+          >
+            Vault
+          </Button>
+          <Button onClick={logoutAndRouteChange} size="sm">
+            Sign Out
+          </Button>
+        </Col>
+      </Row>
+    </SimplePageLayout>
+  );
+};
 export default UserAccount;
