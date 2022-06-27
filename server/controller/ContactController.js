3d0108ebe093d5745d864353278aebe89e9bd884
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-27 12:08:30 -0700

    prettier format contact controller

--- server/controller/ContactController.js
+++ server/controller/ContactController.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
 let router = express();
 
-router.use(bodyParser.json({'limit':'20mb'}));
+router.use(bodyParser.json({ limit: "20mb" }));
 
-router.post("/", (req,res) => {
-    // unpack the form data from the request
-    // name, email, message
-    const {name, email, message} = req.body;
+router.post("/", (req, res) => {
+  // unpack the form data from the request
+  // name, email, message
+  const { name, email, message } = req.body;
 
-    // return a success response
-    res.send({
-        success: true,
-        message: "Message sent successfully"
-    });
+  // return a success response
+  res.send({
+    success: true,
+    message: "Message sent successfully",
+  });
 });
 
 export default router;
