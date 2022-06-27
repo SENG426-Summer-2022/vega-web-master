224cb569ed1914dd994371b4e19a1a6f71590435
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-06-27 13:07:34 -0700

    Adminpanel can now disable users, and shows the current user's role

--- src/components/pages/AdminPanel.js
+++ src/components/pages/AdminPanel.js
@@ -1,54 +1,100 @@
 import SimplePageLayout from '../templates/SimplePageLayout.js';
-import {fetchuser, enableAccount, changeAccountRole} from  '../../service/AdminPanel/AdminPanel.js';
+import { fetchuser, disableAccount, enableAccount, changeAccountRole } from '../../service/AdminPanel/AdminPanel.js';
 import {UserContext} from '../../auth/UserProvider.js';
 import {useState, useContext, useEffect} from 'react';
 
-import {Form, Button, Row, Col, Table} from 'react-bootstrap';
+import {Form, Table} from 'react-bootstrap';
 
-const AdminPanel = (props) => {
+const AdminPanel = (_props) => {
 	const {user} = useContext(UserContext);
 	const [listOfUsers, setUsers] = useState([]);
-	useEffect(() => {
-			console.log("Inside useEffect")
-			fetchuser(user.jwt)
-				.then(resp => {
-					setUsers(resp)
-					});
-		
+	const updateUsers = () => {
+		console.log("Inside useEffect")
+		fetchuser(user.jwt)
+			.then(resp => {
+				setUsers(resp)
+			})
+	}
 
+	useEffect(() => {
+		updateUsers()
 	}, [user]);
 
 	const enableUser = (username) => {
-		console.log("Enable User called with",username)
+		console.log("Enable User called with", username)
 		enableAccount(username, user.jwt)
-		.then(resp => 
-			console.log("User enabled"))
+			.then(_resp => {
+				console.log("User enabled")
+				updateUsers()
+			})
 	}
 
+	const disableUser = (username) => {
+		console.log("Disable User called with", username)
+		disableAccount(username, user.jwt)
+			.then(_resp => {
+				console.log("User disableAccount")
+				updateUsers()
+			})
+	}
 
 	const changeRole = (evt, username) => {
 		console.log(evt.target.value, username)
-		var role = evt.target.value
+		const role = evt.target.value
 		changeAccountRole(username, role, user.jwt)
-		.then(resp => 
-			console.log("Changed Roles"))
+			.then(_resp => {
+				console.log("Changed Roles")
+				updateUsers()
+			})
 	}
+	const updateEnabledState = (user) => user.enabled
+		? disableUser(user.username)
+		: enableUser(user.username);
 
+	// Possible Roles the text to display for it
+	const roles = {
+		"ROLE_STAFF": "STAFF",
+		"ROLE_USER": "USER"
+	}
 	const listOfUsersHTML = () => {
-		if(listOfUsers.length){
-			return listOfUsers.map((user) => <tr><td>{user.firstName}</td><td>{user.lastName}</td><td>{user.username}</td><td onClick={() => enableUser(user.username)}>
-				<a href="#">Enable User</a></td>
+		console.log(listOfUsers)
+		if (!listOfUsers.length) return;
+	    return listOfUsers.map((user) => {
+			console.log(JSON.stringify(user))
+			const auth = user.role.authority ? user.role.authority : "ROLE_USER"
+			// HTML elements for the current users role
+			const RoleText =
+				<option value={auth}>
+					{roles[auth]}
+				</option>;
+
+			// List of HTML elements that are != to the current users role
+			const menuOption = Object.keys(roles)
+				.filter((role) => role != auth)
+				.map((role) =>
+					<option value={role}>
+						{roles[role]}
+					</option>)
+
+			const EnableText = user.enabled
+				? "Disable User"
+				: "Enable User";
+
+			return <tr><td>{user.firstName}</td><td>{user.lastName}</td><td>{user.username}</td>
+				<td onClick={() => updateEnabledState(user)}>
+					<a href="#">{EnableText}</a></td>
 				<td>
-					<Form.Select aria-label="Floating label select example" onChange={(evt) => changeRole(evt, user.username)}>
-					    <option>Open this select menu</option>
-					    <option value="ROLE_STAFF">STAFF</option>
-					    <option value="ROLE_USER">USER</option>
-  					</Form.Select>
-				</td></tr>) 
-		}
+					<Form.Select aria-label="Floating label select example"
+						value={RoleText}
+						onChange={(evt) => changeRole(evt, user.username)}>
+						{RoleText}
+						{menuOption}
+					</Form.Select>
+				</td></tr>
+		})
 	}
 
-	return(
+	return (
 		<SimplePageLayout>
 			<Table>
 				<thead>
@@ -57,7 +103,7 @@ const AdminPanel = (props) => {
 						<td>Last Name</td>
 						<td>Username</td>
 						<td></td>
-						<td></td>
+						<td>Role</td>
 					</tr>
 				</thead>
 				<tbody>
@@ -65,6 +111,6 @@ const AdminPanel = (props) => {
 				</tbody>
 			</Table>
 		</SimplePageLayout>
-		)
+	)
 }
 export default AdminPanel;
