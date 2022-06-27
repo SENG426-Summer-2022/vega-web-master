71f6212523921c3118da139df8f77cf64e97b448
Albie Samson <albie0019@gmail.com>
2022-06-26 20:35:53 -0700

    Added links to Footer Items
    Added links to items in footer nav bar. Those items that do not have it's own page redirects to the main page. Otherwise, links are added to news page and About Us page for the respective fields.

--- src/components/UI/atoms/VenusListGroup.js
+++ src/components/UI/atoms/VenusListGroup.js
@@ -1,6 +1,6 @@
 import {ListGroup} from 'react-bootstrap';
-const VenusListGroup = ({header , list}) => {
-	const listItems = list.map((l, index) => <ListGroup.Item key={index}>{l}</ListGroup.Item>);
+const VenusListGroup = ({header , list, links}) => {
+	const listItems = list.map((l, index) => <ListGroup.Item action href = {links[index]} key={index}>{l}</ListGroup.Item>);
 	return (
 		<ListGroup variant="flush">
 			<ListGroup.Item><b>{header}</b></ListGroup.Item>
