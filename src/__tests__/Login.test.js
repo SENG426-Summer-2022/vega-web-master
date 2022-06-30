45aebcc4e108433f79c0269f4d34e3f8c54a922d
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-30 11:42:21 -0700

    add more login test cases

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -38,15 +38,22 @@ describe("Login", () => {
     });
 
     it("renders the Login page", () => {
-      renderLogin(EMPTY_USER);
+      const { container } = renderLogin(EMPTY_USER);
+
       expect(screen.getByText("Login")).toBeInTheDocument();
+      expect(
+        container.querySelector("a[role='button'][href='/signup']")
+      ).toBeInTheDocument();
     });
 
     it("renders login form", () => {
-      renderLogin(EMPTY_USER);
+      const { container } = renderLogin(EMPTY_USER);
 
       expect(screen.getByLabelText("USERNAME")).toBeInTheDocument();
       expect(screen.getByLabelText("PASSWORD")).toBeInTheDocument();
+      expect(
+        container.querySelector("button[type='submit']")
+      ).toBeInTheDocument();
     });
 
     it("calls login when form is submitted", async () => {
@@ -64,6 +71,69 @@ describe("Login", () => {
       });
 
       expect(mockLogin).toHaveBeenCalledWith(USER_LOGIN);
+    });
+
+    it("displays error message when password field is unfilled", async () => {
+      renderLogin(EMPTY_USER);
+
+      const username = screen.getByLabelText("USERNAME");
+      const submit = screen.getByText("Submit");
+
+      userEvent.type(username, USER_LOGIN.username);
+
+      await act(async () => {
+        await userEvent.click(submit);
+      });
+
+      expect(screen.getByText("Please fill in all fields")).toBeInTheDocument();
+    });
+
+    it("displays error message when username field is unfilled", async () => {
+      renderLogin(EMPTY_USER);
+
+      const password = screen.getByLabelText("PASSWORD");
+      const submit = screen.getByText("Submit");
+
+      userEvent.type(password, USER_LOGIN.password);
+
+      await act(async () => {
+        await userEvent.click(submit);
+      });
+
+      expect(screen.getByText("Please fill in all fields")).toBeInTheDocument();
+    });
+
+    it("displays error message when both username and password fields are unfilled", async () => {
+      renderLogin(EMPTY_USER);
+
+      const submit = screen.getByText("Submit");
+
+      await act(async () => {
+        await userEvent.click(submit);
+      });
+
+      expect(screen.getByText("Please fill in all fields")).toBeInTheDocument();
+    });
+
+    it("displays error message when credentials are incorrect", async () => {
+      mockLogin.mockReturnValue(Promise.resolve({ code: 401 }));
+
+      renderLogin(EMPTY_USER);
+
+      const username = screen.getByLabelText("USERNAME");
+      const password = screen.getByLabelText("PASSWORD");
+      const submit = screen.getByText("Submit");
+
+      userEvent.type(username, USER_LOGIN.username);
+      userEvent.type(password, USER_LOGIN.password);
+
+      await act(async () => {
+        await userEvent.click(submit);
+      });
+
+      expect(
+        screen.getByText("Login failed, The username or password is incorrect.")
+      ).toBeInTheDocument();
     });
   });
 });
