5828b98d080ba0cf42e92cceb7605e4f5a9694b0
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-30 10:21:44 -0700

    add signup test

--- src/__tests__/SignUp.test.js
+++ src/__tests__/SignUp.test.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { UserProvider } from "../auth/UserProvider.js";
 import SignUp from "../components/pages/SignUp.js";
+
+import { MemoryRouter } from "react-router-dom";
 
 const EMPTY_USER = {
   username: "",
@@ -11,30 +14,12 @@ const EMPTY_USER = {
   role: "",
 };
 
-const ADMIN_USER = {
-  username: "admin",
-  jwt: "eyabc",
-  role: "ROLE_ADMIN",
-};
-
-const USER_USER = {
-  username: "user",
-  jwt: "blah",
-  role: "ROLE_USER",
-};
-
-const ADMIN_USER_INFO = {
-  username: "admin",
-  role: "ROLE_ADMIN",
-  firstName: "Vince",
-  lastName: "Vaughn",
-};
-
 const USER_USER_INFO = {
-  username: "user",
+  username: "royobannon@chon.com",
   role: "ROLE_USER",
   firstName: "Roy",
   lastName: "O'Bannon",
+  password: "password",
 };
 
 const signupPromise = Promise.resolve(USER_USER_INFO);
@@ -46,16 +31,90 @@ jest.mock("../service/auth/AuthenticationManager", () => ({
 function renderSignUp(user) {
   return render(
     <UserProvider user={user}>
-      <SignUp />
-    </UserProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={SignUp} />
+        </Switch>
+      </BrowserRouter>
+    </UserProvider>,
+    { wrapper: MemoryRouter }
   );
 }
 
 describe("SignUp", () => {
-  describe("when the user is not logged in", () => {
+  describe("form", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      mockSignup.mockReturnValue(signupPromise);
+    });
     it("renders the SignUp page", () => {
       renderSignUp(EMPTY_USER);
       expect(screen.getByText("Sign Up")).toBeInTheDocument();
+    });
+
+    it("renders signup form", () => {
+      renderSignUp(EMPTY_USER);
+
+      expect(screen.getByLabelText("FIRST NAME")).toBeInTheDocument();
+      expect(screen.getByLabelText("LAST NAME")).toBeInTheDocument();
+      expect(screen.getByLabelText("USERNAME")).toBeInTheDocument();
+      expect(screen.getByLabelText("PASSWORD")).toBeInTheDocument();
+      expect(screen.getByLabelText("CONFIRM PASSWORD")).toBeInTheDocument();
+      expect(screen.getByText("Submit")).toBeInTheDocument();
+    });
+
+    it("renders the form input empty", () => {
+      renderSignUp(EMPTY_USER);
+
+      expect(screen.getByLabelText("FIRST NAME")).toHaveValue("");
+      expect(screen.getByLabelText("LAST NAME")).toHaveValue("");
+      expect(screen.getByLabelText("USERNAME")).toHaveValue("");
+      expect(screen.getByLabelText("PASSWORD")).toHaveValue("");
+      expect(screen.getByLabelText("CONFIRM PASSWORD")).toHaveValue("");
+    });
+
+    it("calls the signup service when the submit button is clicked", async () => {
+      const alertMock = jest
+        .spyOn(window, "alert")
+        .mockImplementation((text) => true);
+
+      renderSignUp(EMPTY_USER);
+      await act(async () => {
+        await userEvent.type(
+          screen.getByLabelText("FIRST NAME"),
+          USER_USER_INFO.firstName
+        );
+        await userEvent.type(
+          screen.getByLabelText("LAST NAME"),
+          USER_USER_INFO.lastName
+        );
+
+        await userEvent.type(
+          screen.getByLabelText("USERNAME"),
+          USER_USER_INFO.username
+        );
+        await userEvent.type(
+          screen.getByLabelText("PASSWORD"),
+          USER_USER_INFO.password
+        );
+        await userEvent.type(
+          screen.getByLabelText("CONFIRM PASSWORD"),
+          USER_USER_INFO.password
+        );
+
+        await userEvent.click(screen.getByText("Submit"));
+      });
+
+      expect(mockSignup).toHaveBeenCalledWith({
+        username: USER_USER_INFO.username,
+        password: USER_USER_INFO.password,
+        firstname: USER_USER_INFO.firstName,
+        lastname: USER_USER_INFO.lastName,
+      });
+
+      expect(alertMock).toHaveBeenCalledWith(
+        "Your account will soon be created"
+      );
     });
   });
 });
