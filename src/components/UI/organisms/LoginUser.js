0cf12dd64c12b947059c2f3d8d1707b8437aa9cc
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-30 10:10:13 -0700

    fix signup, login pages and forms

--- src/components/UI/organisms/LoginUser.js
+++ src/components/UI/organisms/LoginUser.js
@@ -1,41 +1,62 @@
-import {Form, Button, Row, Col} from 'react-bootstrap';
-import React, { useState } from 'react';
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
 
-const LoginUser = ({onSubmit}) => {
+const LoginUser = ({ onSubmit }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	const [username, setUsername]  = useState('');
-	const [password, setPassword] = useState('');
+  const submitForm = (evt) => {
+    evt.preventDefault();
+    onSubmit({
+      username: username,
+      password: password,
+    });
+  };
 
-	const submitForm = (evt) => {
-		evt.preventDefault();
-		onSubmit({
-			'username' : username,
-			'password' : password
-		})
-	}
-
-	return (
-		<Row>
-      		<Col className="mx-auto" xs={6}>
-        		<Form onSubmit={submitForm}>
-	      			<Form.Group className="mb-3">
-	        			<Form.Label>USERNAME</Form.Label>
-	        			<Form.Control type="text" onChange={e => setUsername(e.target.value)}/>
-	      			</Form.Group>
-	      			<Form.Group className="mb-3">
-	        			<Form.Label>PASSWORD</Form.Label>
-	        			<Form.Control type="PASSWORD" onChange={e => setPassword(e.target.value)}/>
-	      			</Form.Group>
-	      			<Button variant="primary" type="submit" onClick={submitForm}>
-	        			Submit
-	      			</Button>
-    			</Form>
-      		</Col>
-		    <p></p>
-			<Button href="/signup">
-				SignUp
-			</Button>
-		</Row>
-		);
-}
+  return (
+    <Container>
+      <Row>
+        <Col className="mx-auto" xs={6}>
+          <Form onSubmit={submitForm}>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="username">USERNAME</Form.Label>
+              <Form.Control
+                id="username"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="password">PASSWORD</Form.Label>
+              <Form.Control
+                id="password"
+                type="PASSWORD"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={submitForm}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="outline-primary"
+          size="lg"
+          href="/signup"
+          style={{ marginTop: "8rem", maxWidth: "20rem" }}
+        >
+          Sign Up
+        </Button>
+      </Row>
+    </Container>
+  );
+};
 export default LoginUser;
