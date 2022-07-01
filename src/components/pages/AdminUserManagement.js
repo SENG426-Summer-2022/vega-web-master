39e6fc8844910001bd815de8869c8e171c8bd5d6
Albie Samson <albie0019@gmail.com>
2022-07-01 12:30:12 -0700

    Updated AdminUserManagement.js
    Working on buttons.

--- src/components/pages/AdminUserManagement.js
+++ src/components/pages/AdminUserManagement.js
@@ -4,29 +4,41 @@ import {UserContext} from "../../auth/UserProvider";
 import {changeAccountRole, enableAccount, fetchuser} from "../../service/AdminPanel/AdminPanel";
 import {Form, Table, Button} from "react-bootstrap";
 
-const AdminUserManagement = (props) => {
+const AdminUserManagement = (props) => { 
 
-    const { context } = props;
-    // of {this.props.location.state.usernamedata}
+    const DeleteUser = () => {
+        console.log(" About to delete User!")
+
+    }
+
+    // Tried this but didn't work: 
+    // <h3>User Management for {this.props.location.state.usernamedata} </h3>
     return(
         <SimplePageLayout>
-
-            <h3> User Management </h3>
+            <h3> User Management for simonl@venus.com </h3>
             <div className={"section1"}>
+                <p></p>
                 <p>Delete this User </p>
-                <Button variant={"danger"}>Delete User</Button>
+                <Button variant={"danger"} onClick={() => DeleteUser}> Delete User </Button>
+                <p></p>
             </div>
             <div className={"section2"}>
+                <p></p>
                 <p>Change User Name </p>
-                <Button variant={"primary"}>Change User Name</Button>
+                <Button variant={"primary"}>Change User Name </Button>
+                <p></p>
             </div>
             <div className={"section3"}>
-                <p>Change User Email</p>
-                <Button variant={"primary"}> Change Email</Button>
+                <p></p>
+                <p>Change User Email </p>
+                <Button variant={"primary"}> Change Email </Button>
+                <p></p>
             </div>
             <div className={"section3"}>
-            <p>Add new User</p>
-            <Button variant={"primary"} href={"../signup"}> Add User</Button>
+                <p></p>
+                <p>Add new User </p>
+                <Button variant={"primary"} href={"../signup"}> Add User </Button>
+                <p></p>
         </div>
         </SimplePageLayout>
     )
