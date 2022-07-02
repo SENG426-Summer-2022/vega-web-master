c8017cba1f8c275f46a203d492fc82e6b3de8d33
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-07-01 19:00:41 -0700

    fix validation, add contact endpoint and api call

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-import { authModule, signupModule } from './auth/AuthenticationManager.js';
-import fileUploader from './controller/FileUploadController.js';
-import adminPanel from './controller/AdminPanelController.js'
-import express from 'express';
-import { config } from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import { authModule, signupModule } from "./auth/AuthenticationManager.js";
+import contact from "./controller/ContactController.js";
+import fileUploader from "./controller/FileUploadController.js";
+import adminPanel from "./controller/AdminPanelController.js";
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8000;
@@ -12,28 +13,28 @@ const env = config();
 
 //app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
   };
   app.use(cors(corsOptions));
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-
 
 app.use("/api/login", authModule);
 // TODO determin how this should be connected
 app.use("/api/signup", signupModule);
-app.use("/api/venus", fileUploader)
-app.use("/api/venus/admin", adminPanel)
+app.use("/api/contact", contact);
+app.use("/api/venus", fileUploader);
+app.use("/api/venus/admin", adminPanel);
 
 app.listen(port, () => {
   console.log(process.env.API_URL);
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
