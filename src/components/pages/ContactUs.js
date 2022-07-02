86efcfd85a3a0560518e5c0e2ce51d75698cb97f
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-07-01 18:58:25 -0700

    generalize form layout

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,10 +1,8 @@
 import FormPageLayout from '../templates/FormPageLayout.js';
-import UserRegistrationForm from '../UI/organisms/UserRegistrationForm.js';
 
 const UserRegistration = (props) => {
 	return (
 		<FormPageLayout>
-			<UserRegistrationForm />
 		</FormPageLayout>
 		);
 }
