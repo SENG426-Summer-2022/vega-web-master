8420d1fa38000f5749ded71b81bfdb33bb70427a
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-07-01 21:08:35 -0700

    add contact page title

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,28 +1,12 @@
 import FormPageLayout from "../templates/FormPageLayout.js";
 import ContactUsForm from "../UI/organisms/ContactUsForm.js";
-
-const onSubmit = async (name, email, message) => {
-  // post to server at localhost:8000/api/contact
-  // send name, email, and message
-  const response = await fetch("http://localhost:8000/api/contact", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name,
-      email: email,
-      message: message,
-    }),
-  });
-  const result = await response.json();
-  return result;
-};
+import CenteredHeader from "../UI/atoms/CenteredHeader.js";
 
 const ContactUs = () => {
   return (
     <FormPageLayout>
-      <ContactUsForm onSubmit={onSubmit} />
+      <CenteredHeader text="Contact Us" />
+      <ContactUsForm />
     </FormPageLayout>
   );
 };
