0a0ebc7f1795f7bf3ceab9f4ae9505d8f7c575ea
Albie Samson <55336018+albie0019@users.noreply.github.com>
2022-07-02 22:06:09 -0700

    Update AdminPanelAPI.js
    removed unnecessary console.logs

--- server/services/AdminPanelAPI.js
+++ server/services/AdminPanelAPI.js
@@ -1,37 +1,27 @@
 import {doPostFile, doGet} from './HTTPRequestAPI.js';
 
 export function fetchusers(url, headers){
-	console.log(headers);
 	return doGet(url, headers['authorization'])
 }
 
 export function enableAccount(url, headers){
-	console.log(headers);
-	console.log(url);
 	return doGet(url, headers['authorization'])
 }
 
 export function changeRole(url,headers){
-	console.log(url);
 	return doGet(url, headers['authorization'])
 }
 
 //New features:
 
 export function deleteAccount(url,headers){
-	console.log(headers);
-	console.log(url);
 	return doGet(url, headers['authorization'])
 }
 
 export function changeEmail(url,headers){
-	console.log(headers);
-	console.log(url);
 	return doGet(url, headers['authorization'])
 }
 
 export function changeUsername(url,headers){
-	console.log(headers);
-	console.log(url);
 	return doGet(url, headers['authorization'])	
 }
