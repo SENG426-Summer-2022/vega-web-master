019f53332dd29f308ddb10780505fafbbfb98303
Albie Samson <albie0019@gmail.com>
2022-07-02 20:56:40 -0700

    Update AdminPanelController.js
    Added new Backend calls to AdminPanel

--- server/services/AdminPanelAPI.js
+++ server/services/AdminPanelAPI.js
@@ -15,3 +15,23 @@ export function changeRole(url,headers){
 	console.log(url);
 	return doGet(url, headers['authorization'])
 }
+
+//New features:
+
+export function deleteAccount(url,headers){
+	console.log(headers);
+	console.log(url);
+	return doGet(url, headers['authorization'])
+}
+
+export function changeEmail(url,headers){
+	console.log(headers);
+	console.log(url);
+	return doGet(url, headers['authorization'])
+}
+
+export function changeUsername(url,headers){
+	console.log(headers);
+	console.log(url);
+	return doGet(url, headers['authorization'])	
+}
