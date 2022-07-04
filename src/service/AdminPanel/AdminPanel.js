40701b7b0ae1b40220f991fb596a1aeda5aede5f
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-07-04 16:53:35 -0700

    Updated VenusNavBar.js and AdminPanel.js
    Removed depreciated userreport link from NavBar, and added new server calls in AdminPanel.js for new features.

--- src/service/AdminPanel/AdminPanel.js
+++ src/service/AdminPanel/AdminPanel.js
@@ -15,3 +15,17 @@ export function disableAccount(username,token){
 export function changeAccountRole(username, role, token){
 	return doGet(`${process.env.REACT_APP_SERVER_URL}/api/venus/admin/changerole?username=`+username+"&role="+role, token)
 }
+
+export function deleteAccount(username, token){
+	console.log("In AdminPAnel: Deleting user" + username)
+	return doGet("http://localhost:8000/api/venus/admin/deleteuser?username="+username, token)
+}
+
+export function updateAccountEmail(username, newEmail, token){
+	return doGet("http://localhost:8000/api/venus/admin/changeemail?username="+username+"&newemail="+newEmail, token)
+}
+
+export function updateAccountusername(username, newUserFirstname, newUserLastname,token){
+	//console.log("In AdminPAnel: Changing user name to" + newUserFirstname + " " + newUserLastname )
+	return doGet("http://localhost:8000/api/venus/admin/changeusername?username="+username+"&newfirstname="+newUserFirstname+"&newlastname="+newUserLastname, token)
+}
