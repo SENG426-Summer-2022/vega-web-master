98312c87c8f1fd0abb7fe4ad268fcc15d874c4ea
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-07-04 17:05:44 -0700

    wip add fe backend support for update user

--- server/services/AdminPanelAPI.js
+++ server/services/AdminPanelAPI.js
@@ -1,27 +1,31 @@
-import {doPostFile, doGet} from './HTTPRequestAPI.js';
+import { doAuthPost, doGet } from "./HTTPRequestAPI.js";
 
-export function fetchusers(url, headers){
-	return doGet(url, headers['authorization'])
+export function fetchusers(url, headers) {
+  return doGet(url, headers["authorization"]);
 }
 
-export function enableAccount(url, headers){
-	return doGet(url, headers['authorization'])
+export function enableAccount(url, headers) {
+  return doGet(url, headers["authorization"]);
 }
 
-export function changeRole(url,headers){
-	return doGet(url, headers['authorization'])
+export function changeRole(url, headers) {
+  return doGet(url, headers["authorization"]);
 }
 
 //New features:
 
-export function deleteAccount(url,headers){
-	return doGet(url, headers['authorization'])
+export function deleteAccount(url, headers) {
+  return doGet(url, headers["authorization"]);
 }
 
-export function changeEmail(url,headers){
-	return doGet(url, headers['authorization'])
+export function changeEmail(url, headers) {
+  return doGet(url, headers["authorization"]);
 }
 
-export function changeUsername(url,headers){
-	return doGet(url, headers['authorization'])	
+export function changeUsername(url, headers) {
+  return doGet(url, headers["authorization"]);
+}
+
+export function updateUser(data, headers) {
+  return doAuthPost("http://localhost:8080/venus/admin/changerole", data, headers);
 }
