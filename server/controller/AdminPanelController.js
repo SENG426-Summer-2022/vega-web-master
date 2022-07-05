b7e89e37670e597929808963b60d0a9c4c529be4
bmazerolle <benmazerolle@yahoo.com>
2022-07-05 02:14:55 -0700

    fix bad url links

--- server/controller/AdminPanelController.js
+++ server/controller/AdminPanelController.js
@@ -19,7 +19,7 @@ router.use(fileUpload({
 
 router.get("/getusers", (req, res) => {
 	console.log("Entered list files");
-	fetchusers(`${process.env.API_URL}/venus/admin/fetchusers`, req.headers)
+	fetchusers("https://seng426group7backend.azurewebsites.net/venus/admin/fetchusers", req.headers)
 	.then(response => {
     	console.log("Response", response);
     	res.send(response);
@@ -34,7 +34,7 @@ router.get("/enableuser", (req, res) => {
 	console.log("Request: Enable User");
 	const {username} = req.query;
 	const {enable} = req.query;
-	enableAccount(`${process.env.API_URL}/venus/admin/enableuser?username=${username}&enable=${enable}`, req.headers)
+	enableAccount(`https://seng426group7backend.azurewebsites.net/venus/admin/enableuser?username=${username}&enable=${enable}`, req.headers)
 	.then(response => {
     	console.log("Response", response);
     	res.send(response);
@@ -49,7 +49,7 @@ router.get("/changerole", (req, res) => {
 	console.log("Request: Change Role")
 	const {username} = req.query;
 	const {role} = req.query;
-	changeRole(`${process.env.API_URL}/venus/admin/changerole?username=${username}&role=${role}`, req.headers)
+	changeRole(`https://seng426group7backend.azurewebsites.net/venus/admin/changerole?username=${username}&role=${role}`, req.headers)
 	.then(response => {
 		console.log("Response", response);
 		res.send(response);
--- server/controller/FileUploadController.js
+++ server/controller/FileUploadController.js
@@ -12,7 +12,7 @@ router.use(fileUpload({
 router.post("/upload", (req,res) => {
 	var formData = req.files;
     console.log("Entered into File uploader", formData)
-    uploader(`${process.env.API_URL}/venus/admin/handlefileupload`, formData, req.headers)
+    uploader("https://seng426group7backend.azurewebsites.net/venus/admin/handlefileupload", formData, req.headers)
     		.then(response => {
     			console.log("Response", response);
     			res.send(response);
@@ -25,7 +25,7 @@ router.post("/upload", (req,res) => {
 
 router.get("/listfiles", (req, res) => {
 	console.log("Entered list files");
-	listFiles(`${process.env.API_URL}/venus/files/listfiles`, req.headers)
+	listFiles("https://seng426group7backend.azurewebsites.net/venus/files/listfiles", req.headers)
 	.then(response => {
     	console.log("Response", response);
     	res.send(response);
@@ -40,7 +40,7 @@ router.get("/fetchcontent", (req, res) => {
 	console.log("Fetch Content")
 	const {name} = req.query
 	console.log(name)
-	fetchcontent(`${process.env.API_URL}/venus/files/fetch/`+name, req.headers)
+	fetchcontent("https://seng426group7backend.azurewebsites.net/venus/files/fetch/"+name, req.headers)
 	.then(response => {
     	console.log("Response", response);
     	res.send(response);
