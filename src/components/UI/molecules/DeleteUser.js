7ab8696d9fa089812b5d2e8c3c5f3978a00f3ff8
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-07-04 17:09:34 -0700

    refactor manage user, add delete confirmation

---
+++ src/components/UI/molecules/DeleteUser.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
+const DeleteUserButton = (props) => {
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  if (showConfirmation) {
+    return (
+      <div>
+        <p>Are you sure you want to delete this user?</p>
+        <div>
+          <Button variant="danger" onClick={() => setShowConfirmation(false)}>
+            Cancel
+          </Button>
+        </div>
+        <div>
+          <Button variant="success" onClick={props.onClick}>
+            Confirm
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Button variant="danger" onClick={() => setShowDeleteConfirmation(true)}>
+        Delete User
+      </Button>
+    </div>
+  );
+};
+
+export default DeleteUserButton;
