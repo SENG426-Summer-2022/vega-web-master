65d7d793ecf7dc1b68d4c659f214e8e2a4fad27d
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-08-03 19:06:43 -0700

    add helmet, xss-clean, compression, rate limiting, csp to express app

--- server/index.js
+++ server/index.js
@@ -1,21 +1,43 @@
 import contact from "./controller/ContactController.js";
 import express from "express";
 import { config } from "dotenv";
+import path from "path";
 import cors from "cors";
 import bodyParser from "body-parser";
-import authController from './controller/AuthController.js';
-import fileUploader from './controller/FileUploadController.js';
-import adminPanel from './controller/AdminPanelController.js';
-import users from './routes/Users.js';
+import compression from "compression";
+import xss from "xss-clean";
+import helmet from "helmet";
+
+import authController from "./controller/AuthController.js";
+import fileUploader from "./controller/FileUploadController.js";
+import adminPanel from "./controller/AdminPanelController.js";
+import users from "./routes/Users.js";
+
+import rateLimiterMiddleware from "./middleware/rateLimiter.js";
 
 const app = express();
 const port = 8000;
 const env = config();
 
-//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
+app.use(compression());
 app.disable("x-powered-by");
+app.use(xss());
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "default-src": ["'self'"],
+      "connect-src": ["'self'", "'unsafe-inline'"],
+      "img-src": ["'self'", "data:"],
+      "style-src-elem": ["'self'", "data:"],
+      "script-src": ["'unsafe-inline'", "'self'"],
+      "object-src": ["'self'"],
+    },
+  })
+);
+app.use(rateLimiterMiddleware);
 
 if (process.env.NODE_ENV === "development") {
   var corsOptions = {
@@ -31,8 +53,10 @@ if (process.env.NODE_ENV === "development") {
   app.use(cors(corsOptions));
 }
 
+app.set("env", process.env.NODE_ENV);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendStatus(200);
 });
 
 app.use("/api/contact", contact);
@@ -42,8 +66,7 @@ app.use("/api/venus", fileUploader);
 app.use("/api/venus/admin", adminPanel);
 app.use("/api/users", users);
 
-
 app.listen(port, () => {
-  console.log(process.env.API_URL);
+  console.log(env.parsed.API_URL);
   console.log(`Example app listening on port ${port}!`);
 });
